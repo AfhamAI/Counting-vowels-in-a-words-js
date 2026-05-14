1ec3e11368bbbc2d5dad826731bdc78ae37fdc30
Afham <afhamtab@gmail.com>
2026-05-14 12:42:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let word = prompt("Enter Your Word");
+let count = 0;
+
+for (let i = 0; i<word.length; i++){
+
+let vow = word[i];
+
+if(
+    vow === "a" || vow === "e" || vow === "i" || vow === "o" || vow === "u")  
+{
+count++;
+}
+
+}
+
+alert(`No: Of Vowels =  ${count}`);
